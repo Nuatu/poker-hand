220d6f898140503cd61ce2277c323a101be5d1b5
Sterling Houghton & Nuatu Tseggai <student@epicodus.com>
2014-07-23 11:22:21 -0700

    sorter function to seperate and sort input

--- lib/poker-hand.rb
+++ lib/poker-hand.rb
@@ -1,11 +1,38 @@
+
+
 def poker_hand (hand)
-  values = []
+  values = [];  numbers = []; suits = []
+  score = 0
 
-  hand.each do |value|
-    values.push([value[0].to_i,value[1]])
-  end
+  numbers = sorter(hand)[0]
+  suits = sorter(hand)[1]
 
+  # # end
+  p numbers
+  p suits
   p values
 end
 
-poker_hand(['2S', '2C', '4D', '5C', '6H'])
+
+def sorter(input)
+  # if input[0].kind_of?(String) == true;
+  #   input[0]
+  numbers = [];  suits = [];  result = []
+
+  input.each do |value|
+    if value.length > 2
+      numbers << (value[0..1].to_i)
+      suits << (value[2])
+    else
+      numbers << (value[0].to_i)
+      suits << (value[1])
+    end
+  numbers.sort!
+  suits.sort!
+  end
+  result << numbers
+  result << suits
+end
+
+poker_hand(['10S', '2C', '4D', '5C', '6H'])
+
